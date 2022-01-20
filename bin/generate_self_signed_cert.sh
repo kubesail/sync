d3de926ac6fac618a5df6b7815a5b129dceee4eb
Seandon Mooy <seandon.mooy@gmail.com>
2022-01-19 16:49:20 -0800

    properly generate der

--- bin/generate_self_signed_cert.sh
+++ bin/generate_self_signed_cert.sh
@@ -7,12 +7,12 @@ if [[ ! -f "${SECRETS_DIR}/tls.crt" || ! -f "${SECRETS_DIR}/tls.key" ]]; then
   mkdir -p ${SECRETS_DIR}
 
   openssl genrsa -out ${SECRETS_DIR}/ca.key 2048
-  openssl req -new -x509 -days 365 -key ${SECRETS_DIR}/ca.key -subj "/C=CN/ST=GD/L=SZ/O=Acme, Inc./CN=Acme Root CA" -out ${SECRETS_DIR}/ca.crt
+  openssl req -new -x509 -days 365 -key ${SECRETS_DIR}/ca.key -subj "/C=US/ST=California/L=Pasadena/O=KubeSail-User/OU=Sync/CN=kubesail-sync.local" -out ${SECRETS_DIR}/ca.crt
 
   openssl req -newkey rsa:2048 -nodes -keyout ${SECRETS_DIR}/tls.key -subj "/C=US/ST=California/L=Pasadena/O=KubeSail-User/OU=Sync/CN=kubesail-sync.local" -out ${SECRETS_DIR}/tls.csr
   openssl x509 -req -extfile <(printf "subjectAltName=DNS:kubesail-sync.local") -days 365 -in ${SECRETS_DIR}/tls.csr -CA ${SECRETS_DIR}/ca.crt -CAkey ${SECRETS_DIR}/ca.key -CAcreateserial -out ${SECRETS_DIR}/tls.crt
 
-  openssl x509 -in ${SECRETS_DIR}/tls.crt -pubkey -noout | openssl rsa -pubin -outform der > ${SECRETS_DIR}/tls.der
+  openssl x509 -outform der -in ${SECRETS_DIR}/tls.crt -out ${SECRETS_DIR}/tls.der
   cat ${SECRETS_DIR}/tls.der | openssl dgst -sha256 -binary | openssl enc -base64 > ${SECRETS_DIR}/pubkey.txt
 
   echo "Done generating certificates"

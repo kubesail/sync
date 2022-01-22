e0575f6e100ddcfb5649db98839e0e7ac74f90a8
Seandon Mooy <seandon.mooy@gmail.com>
2022-01-21 23:56:36 -0800

    match dev yaml to template, add morgan and helmet

--- src/index.js
+++ src/index.js
@@ -2,13 +2,32 @@ const https = require('https')
 const fs = require('fs')
 const express = require('express')
 const multer = require('multer')
+const morgan = require('morgan')
+const helmet = require('helmet')
 const upload = multer({ dest: 'uploads/' })
 
 const app = express()
 const httpsServer = https.createServer(
-  { key: fs.readFileSync('k8s/secrets/tls.key'), cert: fs.readFileSync('k8s/secrets/tls.crt') },
+  {
+    key: fs.readFileSync('k8s/secrets/tls.key'),
+    cert: fs.readFileSync('k8s/secrets/tls.crt'),
+    honorCipherOrder: true,
+  },
   app
 )
+
+app.use(helmet.hsts({ maxAge: 31536000000, includeSubDomains: true, force: true }))
+app.use(
+  morgan(':date[iso] :method :url :status :response-time :res[content-length]', {
+    skip: (req, res) => {
+      return (
+        (req.method === 'OPTIONS' || req.path === '/health' || req.path === '/healthz') &&
+        res.statusCode === 200
+      )
+    },
+  })
+)
+app.disable('x-powered-by')
 
 // SQLite DB
 // On boot-up, scan disk and update SQLite

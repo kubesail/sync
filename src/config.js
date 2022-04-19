fb01c82dc5cc12bff4fd2dd486c182c83e3dcf7d
Seandon Mooy <seandon.mooy@gmail.com>
2022-04-19 12:07:29 -0700

    add file manager capabilities

--- src/config.js
+++ src/config.js
@@ -3,4 +3,5 @@ module.exports = {
     media: process.env.SYNC_DIR_MEDIA || '/home/node/data/media',
     photos: process.env.SYNC_DIR_PHOTOS || '/home/node/data/photos',
   },
+  API_CORS_ALLOWED_ORIGINS: process.env.API_CORS_ALLOWED_ORIGINS || '',
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ const fs = require('fs')
 const path = require('path')
 const { exec } = require('child_process')
 const express = require('express')
+const fsScandir = require('@nodelib/fs.scandir')
 const multer = require('multer')
 const morgan = require('morgan')
 const helmet = require('helmet')
-const { dirs } = require('./config')
+const { dirs, API_CORS_ALLOWED_ORIGINS } = require('./config')
 
 const app = express()
 
@@ -31,9 +32,44 @@ if (!authKey || authKey.length < 8) {
   throw new Error('Invalid SYNC_AUTH_KEY')
 }
 
+if (process.env.NODE_ENV === 'development') {
+  console.log('KubeSail-Sync development mode:', { authKey })
+}
+
 const httpServer = http.createServer(app)
 
 app.use(helmet.hsts({ maxAge: 31536000000, includeSubDomains: true, force: true }))
+const allowedOrigins = API_CORS_ALLOWED_ORIGINS.split(',')
+app.use((req, res, next) => {
+  const allowedCors =
+    API_CORS_ALLOWED_ORIGINS === '*' ? 1 : allowedOrigins.indexOf(req.headers.origin)
+  if (allowedCors > -1) {
+    res.header(
+      'Access-Control-Allow-Origin',
+      API_CORS_ALLOWED_ORIGINS === '*' ? '*' : allowedOrigins[allowedCors]
+    )
+    const allowCredentials = allowedOrigins.includes(allowedOrigins[allowedCors])
+    res.header(
+      'Access-Control-Allow-Credentials',
+      API_CORS_ALLOWED_ORIGINS === '*' || allowCredentials ? 'true' : 'false'
+    )
+  }
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE')
+  res.header('Access-Control-Max-Age', '3600')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'content-type,content-encoding,authorization,x-sync-device,x-sync-key'
+  )
+  res.header('X-Frame-Options', 'SAMEORIGIN')
+  res.header('X-XSS-Protection', '1; mode=block')
+  res.header('Referrer-Policy', 'same-origin')
+  res.header('X-Content-Type-Options', 'nosniff')
+  next()
+})
+
+morgan.token('url', function (req) {
+  return req.url.replace(/x\-sync\-key\=(.*)/, 'x-sync-key=REDACTED')
+})
 app.use(
   morgan(':date[iso] :method :url :status :response-time :res[content-length]', {
     skip: (req, res) => {
@@ -46,15 +82,13 @@ app.use(
 )
 app.disable('x-powered-by')
 app.use((req, res, next) => {
-  if (
-    req.headers['x-sync-device'] &&
-    req.headers['x-sync-key'] &&
-    req.headers['x-sync-key'] === authKey
-  ) {
-    req.device = req.headers['x-sync-device']
-    req.dirs = {
-      photos: path.join(dirs.photos, req.device),
-    }
+  if (req.method === 'OPTIONS') return next()
+  const syncDevice = req.query['x-sync-device'] || req.headers['x-sync-device']
+  const syncKey = req.query['x-sync-key'] || req.headers['x-sync-key']
+
+  if (syncDevice && syncKey && syncKey === authKey) {
+    req.device = syncDevice
+    req.dirs = { photos: path.join(dirs.photos, syncDevice) }
     next()
   } else {
     console.error('Rejected access with a missing x-sync-key')
@@ -92,28 +126,116 @@ app.get('/:tag/check', (req, res) => {
   }
 })
 
-const storage = multer.diskStorage({
-  destination: function (req, _file, cb) {
-    fs.stat(req.dirs.photos, err => {
-      if (!err) {
-        cb(null, req.dirs.photos)
-      } else if (err.code === 'ENOENT') {
-        fs.mkdir(req.dirs.photos, err => {
-          if (!err || err.code === 'EEXIST') cb(null, req.dirs.photos)
-          else throw err
+app.post(
+  '/photos/upload',
+  multer({
+    storage: multer.diskStorage({
+      destination: function (_req, _file, cb) {
+        fs.stat(req.dirs.photos, err => {
+          if (!err) {
+            cb(null, req.dirs.photos)
+          } else if (err.code === 'ENOENT') {
+            fs.mkdir(req.dirs.photos, err => {
+              if (!err || err.code === 'EEXIST') cb(null, destination)
+              else throw err
+            })
+          } else throw err
         })
-      } else throw err
-    })
-  },
-  filename: function (_req, file, cb) {
-    cb(null, file.originalname)
-  },
+      },
+      filename: function (_req, file, cb) {
+        cb(null, file.originalname)
+      },
+    }),
+  }).any(),
+  (req, res) => {
+    console.log(`Uploaded ${req?.files?.length || 0} files to ${req.dirs.photos}`)
+    return res.send({})
+  }
+)
+
+app.post(
+  '/fm/upload/*',
+  multer({
+    storage: multer.diskStorage({
+      destination: function (req, _file, cb) {
+        const targetPath = req.path.replace(/^\/fm\/upload\//, '')
+        const targetPathFull = `/mnt/pvc/${targetPath}`
+        cb(null, targetPathFull)
+      },
+      filename: function (_req, file, cb) {
+        cb(null, file.originalname)
+      },
+    }),
+  }).any(),
+  (req, res) => {
+    console.log(`FileManager: Uploaded ${req?.files?.length || 0} files`)
+    return res.send({})
+  }
+)
+
+app.post('/fm/mkdir/*', (req, res) => {
+  const targetPath = req.path.replace(/^\/fm\/mkdir\//, '')
+  const targetPathFull = `/mnt/pvc/${targetPath}`
+  fs.mkdir(targetPathFull, {}, err => {
+    if (err) return res.send({ error: err.code })
+    return res.send({})
+  })
 })
 
-const uploaderMiddleware = multer({ storage })
-app.post('/photos/upload', uploaderMiddleware.any(), (req, res) => {
-  console.log(`Uploaded ${req?.files?.length || 0} files to ${req.dirs.photos}`)
-  return res.sendStatus(200)
+app.get('/fm/list*', (req, res) => {
+  const targetPath = req.path.replace(/^\/fm\/list\//, '')
+  const targetPathFull = `/mnt/pvc/${targetPath}`
+  fsScandir.scandir(targetPathFull, { stats: true }, (err, stats) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        return res.send({ error: 'ENOENT' })
+      }
+      console.error('Failed to list dir', { targetPathFull, err })
+      return res.sendStatus(500)
+    }
+    const files = stats.map(s => {
+      const isDir = s.dirent.isDirectory()
+      return {
+        id: `${targetPath}/${s.name}`,
+        name: s.name,
+        isDir,
+        ext: s.name.includes('.') ? undefined : '',
+        size: isDir ? undefined : s.stats.size,
+        modDate: s.stats.mtime,
+      }
+    })
+    return res.send({ files })
+  })
+})
+
+app.get('/fm/delete*', (req, res) => {
+  const targetPath = req.path.replace(/^\/fm\/delete\//, '')
+  const targetPathFull = `/mnt/pvc/${targetPath}`
+  if (targetPath === '/' || targetPath === '') return res.sendStatus(400)
+  fs.unlink(targetPathFull, err => {
+    if (err) {
+      if (err.code === 'ENOENT') return res.send({ error: 'ENOENT' })
+      console.error('Failed to unlink', { targetPathFull, err })
+      return res.sendStatus(500)
+    }
+    return res.send({})
+  })
+})
+
+app.get('/fm/download*', (req, res) => {
+  const targetPath = req.path.replace(/^\/fm\/download\//, '')
+  const targetPathFull = `/mnt/pvc/${targetPath}`
+  const fileStream = fs.createReadStream(targetPathFull)
+  fileStream.on('open', () => {
+    // req.headers['content-type']
+    res.attachment(path.basename(targetPath))
+    fileStream.pipe(res)
+  })
+  fileStream.on('error', err => {
+    if (err.code === 'ENOENT') return res.send({ error: 'ENOENT' })
+    console.error('Failed to download file', { targetPathFull, err })
+    return res.sendStatus(500)
+  })
 })
 
 if (httpsServer) {
